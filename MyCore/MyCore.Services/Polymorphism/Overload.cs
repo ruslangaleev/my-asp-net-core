2d3b4cb6422a95b592a44390135ceafc3478dccc
Ruslan Galeev <ruslan.nafisovich@gmail.com>
2021-02-01 12:11:04 +0500

    Добавлены примеры по работе с полиморфизмом

---
+++ MyCore/MyCore.Services/Polymorphism/Overload.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCore.Services.Polymorphism
+{
+    /*
+     * Полиморфизм - свойство одних и тех объектов и методов принимать разные формы:
+     *  1. Перегрузка методов - совершают одинаковые действия с разными типами данных
+     *      Метод не идентифицируется по возвращаемому типу, с модификатором static
+     *      Метод идентифицируется только по имени, типу и количеству паарметров
+     */
+
+    public class Overload
+    {
+        public int DisplayOverload(int a) {
+            return a;
+        }
+
+        public int DisplayOverload(string a) {
+            var result = 0;
+            int.TryParse(a, out result);
+            return result;
+        }
+
+        public int DisplayOverload(string a, int b) {
+            var result = 0;
+            int.TryParse(a, out result);
+
+            result = result + b;
+
+            return result;
+        }
+
+        // статика не пойдет.
+        //public static int DisplayOverload(int a) {
+
+        //}
+
+        // метод с оператором out/ref будет работать.
+        public static int DisplayOverload(out int a)
+        {
+            a = 5;
+
+            return a + 5;
+        }
+
+        // метод конфликтует с другими по имени и сигнатуре
+        //public int DisplayOverload(out int a)
+        //{
+        //    a = 5;
+
+        //    return a + 5;
+        //}
+
+        // здесь массивы, благодаря params, передаются по ссылке
+        public int DisplayOverload(params int[][] p)
+        {
+            var result = 0;
+
+            foreach (var item1 in p)
+            {
+                foreach (var item2 in item1)
+                {
+                    result = result + item2;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MyCore/MyCore.UnitTests/Polymorphism/OverloadTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyCore.Services.Polymorphism;
+using Xunit;
+
+namespace MyCore.UnitTests.Polymorphism
+{
+    public class OverloadTests
+    {
+        [Fact]
+        public void TestA()
+        {
+            var overload = new Overload();
+
+            var result = overload.DisplayOverload(1);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void TestB()
+        {
+            var overload = new Overload();
+
+            var value = 5;
+
+            // статика недоступна!
+            //var result = overload.DisplayOverload(out value);
+            var result = Overload.DisplayOverload(out value);
+
+            Assert.Equal(5, value);
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void TestC()
+        {
+            var overload = new Overload();
+
+            var p1 = new int[] {1, 2, 3};
+            var p2 = new int[] {4, 5, 6};
+
+            var result = overload.DisplayOverload(p1, p2);
+
+            Assert.Equal(21, result);
+        }
+    }
+}
